c270d0dbe832c7bad2cb5eeb474e46f5a26f3c81
Masahiro Sakai <masahiro.sakai@gmail.com>
2019-02-02 14:22:26 +0900

    use __dealloc__ instead of __del__ for cupy.cudnn.Descriptor

--- cupy/cudnn.pyx
+++ cupy/cudnn.pyx
@@ -85,7 +85,7 @@ cdef class Descriptor:
         self.value = descriptor
         self.destroy = destroyer
 
-    def __del__(self):
+    def __dealloc__(self):
         if self.value:
             self.destroy(self.value)
             self.value = 0
